1b026607ec4884cbad4ff92f8a9b3a4dbb299eda
Sreekar <mandapallisreekar@gmail.com>
2025-12-30 16:51:03 +0530

    Add Container layout, Navbar, and About page

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import Container from "../../components/Container";
+
+export default function About() {
+  return (
+    <Container>
+      <h1>About</h1>
+      <p>
+        Prof. Rimjhim is a faculty member at the National Institute of Technology
+        Warangal. Her academic interests include data-driven and analytical
+        approaches in computer science.
+      </p>
+    </Container>
+  );
+}
---
+++ src/components/Container.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Container({ children }) {
+  return (
+    <div style={{ maxWidth: "900px", margin: "0 auto", padding: "20px" }}>
+      {children}
+    </div>
+  );
+}
